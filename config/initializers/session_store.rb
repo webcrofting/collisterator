95f455951551b1de957949e179d5fdeadc386dea
Mecha Raptoris <mecharaptors@gmail.com>
2012-07-26 20:33:40 -0400

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Collisterator::Application.config.session_store :cookie_store, key: '_collisterator_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Collisterator::Application.config.session_store :active_record_store
