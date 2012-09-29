d6d36086e3259ae90dc28c569d07fac2a93abbcf
Mecha Raptoris <mecharaptors@gmail.com>
2012-09-29 16:42:03 -0400

    Changed item_to_json_hash. Now adds id and data to a hash, creates a children array, and recursively calls itself to get the hash for each child and add it to the array. Currently doesnt use any helper methods of its own?

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,50 +1,23 @@
 module ItemsHelper
-	def item_to_json_hash()
-		all_roots = Item.roots
-		item_arr = []
+
+	def item_to_json_hash(item)
 		
-		all_roots.each_with_index do |item, index|
-			if (item.has_children?)
-				c = item.children
-				item_arr << add_children(item.id, c)
-			else
-				item_arr << add_children(item.id, nil)
-			end
-		end
-			
-		return item_arr
-		
-	end
-	
-	
-	def add_children(parent_id, all_children) 
-	
-		data = {:item_id => parent_id, :data => get_item_data(parent_id)}
+		data = {:item_id => item.id, :data => item.data}
 		
 		children = []
 		
-		unless all_children.nil?
-			
-				all_children.each_with_index do |item, index|
-				if (item.has_children?)
-					c = item.children
-					children << add_children(item.id, c)
-				else
-					children << add_children(item.id, nil)
-				end
+		if (item.has_children?)
+			children_collection = item.children
+			children_collection.each do |child|
+				children << item_to_json_hash(child)
 			end
 			
 			data[:children] = children
-		
-		end
-		
+		end	
 		
 		
-		return data
 	end
 	
-	def get_item_data(id)
-		@item = Item.find(id)
-		return @item.data
-	end
+	
+	
 end
