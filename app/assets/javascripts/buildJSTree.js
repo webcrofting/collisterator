15c1df9479e901d0b148994a61cd9a1546fb94d6
Mecha Raptoris <mecharaptors@gmail.com>
2012-10-10 19:40:14 -0400

    Fixed call to buildJSTree.js so the url to the json for items can be created based on the given item id.

--- app/assets/javascripts/buildJSTree.js
+++ app/assets/javascripts/buildJSTree.js
@@ -1,24 +1,125 @@
-var JSTree = function(nodeNumber) {
+var item_id_number; 
 
-	if (nodeNumber > 0) {
+var buildJSTree = function(item_id_number) {
+	this.item_id_number = item_id_number;
+}
+
+var json_url = function() {
+	if (item_id_number===0) { // using this for "root of all nodes"
+		return "items.json"; // only works in the index.
+	} else if (item_id_number > 0) {
+		return "/items/" + item_id_number + ".json";
+	}
+}
+
+
+$(document).ready(function(){
+	
+	$("#collisterator_tree").jstree({
+		core : { "html_titles" : true},
+		themes : { "theme" : "classic" },
+		"json_data" : {
+			"ajax" : { 
+				"url" : json_url,
+				"data" : function(m) {
+						var html_data = m.attr								
+						return { id : html_data ? m.attr("id") : 0 };
+					}
+			}
+		},
+		"contextmenu" : {
+			
+			"rename" : {
+					"label" : "rename",
+					"action" : function(obj) { this.rename(obj) }
+			},
+			"remove" : {
+				"label" : "Delete",
+				"action" : function(obj) { this.remove(obj) }
+			},
+			"create" : {
+				"label" : "New Item",
+				"action" : function(obj) { this.create(obj) }
+			}	
+		
+		},
+		plugins : ["themes", "json_data", "contextmenu"]
+	})
+});
+
+/* Something like what I want?
+function buildJSTree(item_id_number) {
+	
+	
+	$(document).ready(function(){
 		$("#collisterator_tree").jstree({
-			core : { "html_titles" : true },
+			core : { "html_titles" : true},
 			themes : { "theme" : "classic" },
+			plugins : ["themes", "json_data", "CRRM", "contextmenu"]
+
 			"json_data" : {
 				"ajax" : { 
-					"url" : "/items/" + nodeNumber + ".json",
+					"url" : json_url,
 					"data" : function(m) {
 							var html_data = m.attr								
 							return { id : html_data ? m.attr("id") : 0 };
 						}
 				}
 			},
-			plugins : ["themes", "json_data"]
+			
+			"crrm" : {
+				"input_width_limit" : 2000
+			},
+			"contextmenu" : {
+				"rename" : {
+					"label" : "Edit",
+					"action" : function(obj) { this.rename(obj) },
+				}
+				"remove" : {
+					"label" : "Delete",
+					"action" : function(obj) { this.remove(obj) },
+					
+				}
+				"create" : {
+					"label" : "New Item",
+					"action" : function(obj) { this.create(obj) },
+				}
+			
+			},
 		})
-	}
-} 
+	});
+
+}
+*/
 
 /**
+Saw it written in stackoverflow as: 
+	}).bind("rename.jstree create.jstree remove.jstree", function(event, data) {
+		var type = event.type;
+		alert(type)
+		if (type === 'rename.jstree') {
+			// handle move_node here
+		} else if (type === 'create.jstree') {
+			//handle rename jstree
+		} else if (type === 'remove.jstree') {
+			//handle create
+		}
+		
+				$("#create_button").click(function() {
+			$("collisterator_tree").jstree(null, "last", {data:title, attr: {id :item_id}}, null, false);
+		}
+
+		// blah blah blah close brackets
+
+*/ 
+
+/* This runs, but it never references buildJSTree and when I call it, I'm getting a "Reference 
+   Error" that it is undefined...? 
+   
+   Also : url should be /items.json if in show view & items.json if on index.
+
+
+
 $(document).ready(function(){
 	$("#collisterator_tree").jstree({
 		core : { "html_titles" : true},
@@ -35,4 +136,5 @@ $(document).ready(function(){
 		plugins : ["themes", "json_data"]
 	})
 });
+
 */
