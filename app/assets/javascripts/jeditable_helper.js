ea1542a1fd19e30b9650c45cc6d8081d8bf91899
Mecha Raptoris <mecharaptors@gmail.com>
2012-10-26 13:57:25 -0400

    Rewrote jeditable_helper to act as a helper for Collisterator.renderNodeContent.

--- app/assets/javascripts/jeditable_helper.js
+++ app/assets/javascripts/jeditable_helper.js
@@ -1,7 +1,26 @@
-$(document).ready(function() {
-	$('.editable').editable('/items/1', {
+$('.editable').editable('/items/1', {
 			method: "PUT",
 			submit: 'OK',
 			name: 'item[data]'
-	});
 });
+Helper = 
+	{
+		getNewNode : function(node_id) 
+			{
+				return '<a href="/items/new?parent_id=' + node_id + '">New Child of Item</a>';
+			},
+		getDeleteNode: function(node_id)
+			{
+				return "<form class='button_to' method='post' action='/items/" + node_id + "' data-remote='true' onsubmit='window.location.reload()'><div><input name='_method' value='delete' type='hidden' /><input value='Destroy' type='submit' disable_with='loading...' data-confirm='Are you sure?' /></div></form>";
+			},
+		loadEditable : function(node_id) 
+			{
+				var urlForJeditable = '/items/' + node_id;
+				$('.editable').editable(urlForJeditable, {
+						method: 'PUT',
+						submit: 'OK',
+						name: 'item[data]'
+				});
+			},
+	}
+	
