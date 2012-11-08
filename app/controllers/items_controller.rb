b358f1cc48c46eaeb6be150d889a557b5e85d13b
Mecha Raptoris <mecharaptors@gmail.com>
2012-11-08 16:58:51 -0500

    Removed redirect from item_controllers' destroy action.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -80,18 +80,12 @@ class ItemsController < ApplicationController
   # DELETE /items/1.json
   def destroy
     @item = Item.find(params[:id])
-
-    
+	
     @item.children.each do |child|
         child.parent_id = nil
     end
 
-
     @item.destroy
 
-    respond_to do |format|
-      format.html { redirect_to items_url }
-      format.json { head :no_content }
-    end
   end
 end
