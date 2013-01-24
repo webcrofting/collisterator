1e2090031af9d2f383f457a080e4afd208dd6948
Mecha Raptoris <mecharaptors@gmail.com>
2013-01-24 09:55:31 -0500

    Removed item_to_json_hash from items_helper.

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,23 +1,3 @@
 module ItemsHelper
-
-	def item_to_json_hash(item)
-		
-		data = {:item_id => item.id, :list_type_id => item.list_type_id, :data => item.data}
-		
-		children = []
-		
-		if (item.has_children?)
-			children_collection = item.children
-			children_collection.each do |child|
-				children << item_to_json_hash(child)
-			end
-			
-			data[:children] = children
-		end	
-		
-		return data
-	end
-	
-	
 	
 end
