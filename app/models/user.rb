79d5e08458a153b44f4033c51db78b9a20352f51
Mecha Raptoris <mecharaptors@gmail.com>
2013-02-16 20:32:24 -0500

    Users are now set up with a default role in the db.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,14 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
   
   has_and_belongs_to_many :roles
+  before_create :set_default_role
   
   def role?(role)
     return !!self.roles.find_by_name(role.to_s)
   end
   
+  private
+  def set_default_role
+    self.role ||= Role.find_by_name('players');
+  end
 end
