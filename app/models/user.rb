1acbeac75b10268443457c771c74f9bada9ee973
Mecha Raptoris <mecharaptors@gmail.com>
2013-03-04 13:31:19 -0500

    Still trying to get Omniauth to work.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [ :google_oauth2]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :roles
@@ -12,6 +12,26 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :roles
   before_create :set_default_role
   
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => access_token.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(name: data["name"],
+          provider:access_token.provider,
+          email: data["email"],
+          uid: access_token.uid ,
+          password: Devise.friendly_token[0,20],
+        )
+      end
+   end
+end
+
   def role?(role)
     return !!self.roles.find_by_name(role.to_s)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Collisterator::Application.routes.draw do
 
   devise_for :users, :path_names => {:sign_in => "login", :sign_out => "logout"}, :path => "d"
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   resources :users
   resources :list_types
