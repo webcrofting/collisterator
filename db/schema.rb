34052d49aeee6d951832c8b990e6a9ddea109cdc
Mecha Raptoris <mecharaptors@gmail.com>
2013-05-14 21:42:53 -0400

    Changed items to work as a nested resource of users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130418005117) do
+ActiveRecord::Schema.define(:version => 20130515011719) do
 
   create_table "item_hierarchies", :id => false, :force => true do |t|
     t.integer "ancestor_id",   :null => false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20130418005117) do
     t.integer  "list_type_id"
     t.string   "token"
     t.string   "username"
+    t.integer  "user_id"
   end
 
   create_table "list_types", :force => true do |t|
