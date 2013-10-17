10fb7ebf2df8ad9835dfc22dc9ac4d5df3c966d8
Anne Geiersbach <annasazi@gmail.com>
2013-10-17 14:17:57 -0400

    Fixed form for sharing lists on users profile pages.

--- app/controllers/item_shares_controller.rb
+++ app/controllers/item_shares_controller.rb
@@ -2,20 +2,18 @@ class ItemSharesController < ApplicationController
 	
 	def new
 		@item_share = ItemShare.new
-		respond_with @item_share
-		#is that even necessary???
 	end
 	
 	def create
-		@shared_user_email = params[:shared_user_email]
-		logger.debug "#{@shared_user_email}"
-		respond_to do |format|
-			if @item_share.save
-				format.html {redirect_to user_path(current_user.id), :notice => "List shared successfully"}
-			else
-				flash.now[:alert] = "Could not submit new share list form."
-			end
-		end
-	end
+    @item_share = ItemShare.new params[:item_share]
+    if @item_share.save
+      flash[:notice] = "Item successfully shared."
+      redirect_to current_user
+    else
+      flash[:error] = "Error sharing list."
+      flash[:share_params] = params[:item_share]
+      redirect_to :action => :new
+    end
+  end
 	
 end
