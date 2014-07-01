967136058f570ec7f2daf0c1a37fe42db7413e03
ageiersbach <annasazi@gmail.com>
2014-07-01 10:35:18 -0400

    Install DatabaseCleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,11 +9,28 @@ RSpec.configure do |config|
 
 	config.include FactoryGirl::Syntax::Methods
 
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   config.infer_spec_type_from_file_location!
 
 	config.before(:suite) do
 		FactoryGirl.lint
+		DatabaseCleaner.clean_with(:truncation)
+	end
+
+	config.before(:each) do
+		DatabaseCleaner.strategy = :transaction
+	end
+
+	config.before(:each, :js => true) do
+		DatabaseCleaner.strategy = :truncation
+	end
+
+	config.before(:each) do
+		DatabaseCleaner.start
+	end
+
+	config.after(:each) do
+		DatabaseCleaner.clean
 	end
 end
