1a4156feeedfeb15b06156c72ec8154c52877a5a
ageiersbach <annasazi@gmail.com>
2014-07-06 16:21:21 -0400

    Finish ListType spec

--- spec/features/creating_list_types_spec.rb
+++ spec/features/creating_list_types_spec.rb
@@ -11,15 +11,39 @@ feature "Creating ListTypes", js: true do
 	scenario "an admin can create a ListType" do
 		admin = create(:user, :role => "admin") 
 		ltp.login(admin)
-		click_link 'New List Type'	
-		fill_in 'List Type Name', :with => "Voter List"
-		click_button "Add Field For List"
-		sleep 1
-		save_and_open_screenshot
+		ltp.make_new_list_type
+		ltp.name_list_type('Voter List')
+		
+		field = { :name => "candidate", :data => "Joe Politician" }
+		ltp.add_field(field)
+
+		ltp.save_list_type
+		expect(ltp).to have_saved_list_type
+		expect(ltp).to have_saved_fields([field])	
 	end
 
-	scenario "a payer can create a ListType"
+	scenario "a payer can create a ListType" do
+		payer = create(:user, :role => "payer")
+		ltp.login(payer)
+
+		ltp.make_new_list_type
+		ltp.name_list_type('Baby Animals')
+		
+		field = { :name => "name", :data => "capybara" }
+		field1 = { :name => "url", :data => "http://gianthampster.com" }
+		ltp.add_field(field)
+		ltp.add_field(field1)
+
+		ltp.save_list_type
+		expect(ltp).to have_saved_list_type
+		expect(ltp).to have_saved_fields([field, field1])
+	end
 	
-	scenario "a player cannot create a ListType"
+	scenario "a player cannot create a ListType" do
+		player = create(:user, :role => "player")
+		ltp.login(player)
+		ltp.make_new_list_type
+		expect(page).to have_content("You are not authorized to access this page.")
+	end
 end
 
--- spec/support/pages/list_type_page.rb
+++ spec/support/pages/list_type_page.rb
@@ -1,7 +1,51 @@
 class ListTypePage < Page
+
+	def initialize
+		@fields = 0
+	end
 	
 	def open
 		visit '/'
 	end
 
+	def make_new_list_type
+		click_link 'New List Type'	
+	end
+
+	def name_list_type(name)
+		fill_in 'List Type Name', :with => name
+	end
+
+	def add_field(field)
+		click_button "Add Field For List"
+		field_name = page.find("#field-name#{@fields}")
+		field_name.set(field[:name])
+		field_data = page.find("#field-data#{@fields}")
+		field_data.set(field[:data])
+		increment_fields
+	end
+
+	def save_list_type
+		click_button "Save"	
+	end
+
+	def has_saved_list_type?
+		page.has_content?("List type was successfully created")
+	end
+
+	def has_saved_fields?(fields)
+
+		fields.each do |field|
+			unless page.has_content?(field[:name]) && 
+			  page.has_content?(field[:data]) 
+					return false
+			end
+		end
+		true
+	end
+
+private
+	def increment_fields
+		@fields += 1		
+	end	
 end
