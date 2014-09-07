df76352d7532e4dd482bcb1ac0fdae86bd07faac
ageiersbach <annasazi@gmail.com>
2014-09-07 19:11:04 -0400

    Add examples for ItemSharesController

--- spec/controllers/items_shares_controller_spec.rb
+++ spec/controllers/items_shares_controller_spec.rb
@@ -1,8 +1,58 @@
 require 'rails_helper'
 describe ItemSharesController do
-	
-	describe "#create" do
-		it "creates a new ItemShare" do
-		end
+  let!(:email) { 'albion@example.com' }
+  let!(:item) { create(:item) }
+
+  describe "#create" do
+    
+    let(:params) { { owner_id: owner.id, item_id: item.id, shared_user_email: email } } 
+    
+    context "with valid params"  do
+      
+      context "as an admin" do
+        let(:owner) { create(:user, role: 'admin') }
+        it_behaves_like "an authorized ItemShares creator"
+      end
+
+      context "as a payer" do
+        let(:owner) { create(:user, role: 'payer') }
+        it_behaves_like "an authorized ItemShares creator"
+      end
+
+      context "as a player" do
+        let(:owner) { create(:user, role: 'player') }
+        it_behaves_like "an authorized ItemShares creator"
+      end
+
+      #TODO: this should have different behavior. unauthorized users do not have profiles
+      context "unauthorized" do
+        let(:owner) { create(:user, role: nil) }
+        it_behaves_like "an authorized ItemShares creator"
+      end
+    end
+
+    context "with invalid params" do
+      let(:params) { { owner_id: owner.id, item_id: nil, shared_user_email: email } }
+
+      context "as an admin" do
+        let(:owner) { create(:user, role: 'admin') }
+        it_behaves_like "an invalid ItemShares creation"
+      end
+
+      context "as a payer" do
+        let(:owner) { create(:user, role: 'payer') }
+        it_behaves_like "an invalid ItemShares creation"
+      end
+
+      context "as a player" do
+        let(:owner) { create(:user, role: 'player') }
+        it_behaves_like "an invalid ItemShares creation"
+      end
+
+      context "unauthorized" do
+        let(:owner) { create(:user, role: nil) }
+        it_behaves_like "an invalid ItemShares creation"
+      end
+    end
 	end
 end
---
+++ spec/controllers/shared_examples/item_shares_examples.rb
@@ -0,0 +1,35 @@
+shared_examples_for "an authorized ItemShares creator" do
+  before(:each) { sign_in owner } 
+  
+  it "assigns a new @item_share" do
+    post :create, item_share: params
+    expect(assigns(:item_share)).to be_a ItemShare
+  end
+
+  it "updates the @item_share attributes" do
+    post :create, item_share: params
+    expect(assigns(:item_share).owner_id).to eq(owner.id)
+    expect(assigns(:item_share).item_id).to eq(item.id)
+  end
+
+  it "creates the @item_share" do
+    expect {
+      post :create, item_share: params
+    }.to change(ItemShare, :count).by(1)
+  end
+
+  it "redirects to the profile" do
+    post :create, item_share: params
+    expect(response).to redirect_to profile_path(owner)
+    expect(flash[:notice]).to eq("Item successfully shared.")
+  end
+end
+
+shared_examples_for "an invalid ItemShares creation" do
+  #TODO: this should handle failure more gracefully
+  it "throws an error" do
+    expect {
+      post :create, item_share: params
+    }.to raise_error
+  end
+end
