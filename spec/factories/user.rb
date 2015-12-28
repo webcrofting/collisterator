f268d0af0186cbc1865429c0d8f771f0a38b795d
Anne G <annasazi@gmail.com>
2015-12-27 23:23:24 -0500

    Use sequence for user factory's email

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,10 @@
-FactoryGirl.define do 
-	factory :user, class:User do				
-		password							'password'
-		password_confirmation 'password'
-		email									'user@example.com'		
-	end
+FactoryGirl.define do
+  factory :user, class:User do
+    password              'password'
+    password_confirmation 'password'
+
+    sequence :email do |n|
+      "user#{n}@example.com"
+    end
+  end
 end
